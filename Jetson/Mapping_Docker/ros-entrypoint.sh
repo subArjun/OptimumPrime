e9f1cc057e1147689ea6ef588cec74ce80ac7b58
subArjun <118311762+subArjun@users.noreply.github.com>
2024-03-25 17:18:52 -0400

    Update ros-entrypoint.sh

--- Jetson/Mapping_Docker/ros-entrypoint.sh
+++ Jetson/Mapping_Docker/ros-entrypoint.sh
@@ -4,6 +4,8 @@ set -e
 # source ROS 2 setup files
 source "/opt/ros/foxy/setup.bash"
 source "/ros2_ws/install/setup.bash"
+export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
+export ROS_DOMAIN_ID=2
 
 ros2 launch push_mapping push_mapping.launch.py
 # execute the passed command
